bf80c54ac1365ff02e59cd8379f870fb17340413
sandhiya12 <sandhiya0807@gmail.com>
2016-06-07 04:33:08 -0700

    Create check.c

---
+++ check.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int n;
+	scanf("%d",&n);
+	if(n==0)
+	printf("Zero");
+	else if(n>0)
+	printf("Positive");
+	else
+     printf("Negative");
+	return 0;
+}
